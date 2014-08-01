ceabb5ce38cd68f5ebbdfc20f3cc81dd4ef575b0
Nicolas Deveaud <nicolas.deveaud@francetv.fr>
2014-08-01 02:47:39 +0200

    set first test suite

--- test/mem.test.js
+++ test/mem.test.js
@@ -2,8 +2,120 @@
     function factory(mem, chai, sinon, mocha) {
 
         return describe('mem', function() {
-            it ('should be available', function() {
-                chai.expect(sample_module).to.eql({sample:'module'});
+            afterEach(function() {
+                mem._callbacks = [];
+
+                if (console.error.restore) {
+                    console.error.restore();
+                }
+            });
+
+            it ('should trigger events', function(done) {
+                var subject = {};
+                mem.on(subject, 'event', function() {
+                    done();
+                });
+
+                mem.trigger(subject, 'event');
+            });
+
+            it ('should trigger events on all listeners', function(done) {
+                var subject = {};
+
+                function count() {
+                    count.val = (count.val || 0) + 1;
+
+                    if (count.val === 2) {
+                        done();
+                    }
+                }
+
+                mem.on(subject, 'event', function() {
+                    count();
+                });
+
+                mem.on(subject, 'event', function() {
+                    count();
+                });
+
+                mem.trigger(subject, 'event');
+            });
+
+            it ('should trigger events on all listeners execept "offed" ones', function(done) {
+                var subject = {};
+
+                function count() {
+                    count.val = (count.val || 0) + 1;
+
+                    if (count.val === 2) {
+                        done();
+                    }
+                }
+
+                function count2() {
+                    count();
+                }
+
+                mem.on(subject, 'event', count);
+
+                mem.on(subject, 'event', count);
+
+                mem.on(subject, 'event', count2);
+                mem.off(subject, 'event', count2);
+
+                mem.trigger(subject, 'event');
+            });
+
+            it ('off will remove ALL matching listeners', function(done) {
+                var subject = {};
+
+                function count() {
+                    count.val = (count.val || 0) + 1;
+
+                    if (count.val === 2) {
+                        done();
+                    }
+                }
+
+                function count2() {
+                    count();
+                }
+
+                mem.on(subject, 'event', count);
+
+                mem.on(subject, 'event', count);
+
+                mem.on(subject, 'event', count2);
+                mem.on(subject, 'event', count2);
+
+                mem.off(subject, 'event', count);
+
+                mem.trigger(subject, 'event');
+            });
+
+            it ('An error in a listener won\'t break all listeners', function(done) {
+                sinon.stub(console, 'error');
+
+                var subject = {};
+
+                function count() {
+                    count.val = (count.val || 0) + 1;
+
+                    if (count.val === 2) {
+                        chai.expect(console.error.called).to.equal(true);
+                        done();
+                    }
+                }
+
+                function count2() {
+                    throw new Error('sample error');
+                }
+
+                mem.on(subject, 'event', count);
+                mem.on(subject, 'event', count2);
+                mem.on(subject, 'event', count);
+
+                mem.trigger(subject, 'event');
             });
         });
 
